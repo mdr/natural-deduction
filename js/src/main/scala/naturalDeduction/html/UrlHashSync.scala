8c043c44735e2ae416574b0122407215de9657d1
Matt Russell <MattRussellUK@gmail.com>
2021-05-08 13:55:33 +0100

    wip

--- js/src/main/scala/naturalDeduction/html/UrlHashSync.scala
+++ js/src/main/scala/naturalDeduction/html/UrlHashSync.scala
@@ -5,11 +5,13 @@ import naturalDeduction.parser.FormulaParser.parseDerivations
 import naturalDeduction.pretty.DerivationSerialiser
 import org.scalajs.dom.window
 
+import scala.scalajs.js.URIUtils
+
 object UrlHashSync {
 
   def readFromHash: Seq[Derivation] = window.location.hash match {
     case "" => Seq.empty
-    case s => parseDerivations(s.substring(1))
+    case s => parseDerivations(URIUtils.decodeURIComponent(s.substring(1)))
   }
 
   def writeToHash(derivations: Seq[Derivation]): Unit = window.location.hash = DerivationSerialiser.serialise(derivations)
